f0d0aa5872ca5b048768f073c2b3b93d29aa94a0
Ha Nguyen <haa27122004@gmail.com>
2023-10-21 00:48:42 +0700

    fix validate

--- src/main/java/com/example/dachothuenha/Controller/UserRegisterAsHost.java
+++ src/main/java/com/example/dachothuenha/Controller/UserRegisterAsHost.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 
 @WebServlet(name = "UserRegisterAsHost", value = "/register_Host")
 public class UserRegisterAsHost extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
@@ -20,13 +21,49 @@ public class UserRegisterAsHost extends HttpServlet {
         String confirmPassword = req.getParameter("confirm-password");
         User newUser = new User(username, phone, password);
         UserService userService = new UserService();
+
+        boolean isNumeric = true;
+        for (int i = 0; i < phone.length(); i++) {
+            char c = phone.charAt(i);
+            if (!Character.isDigit(c)) {
+                isNumeric = false;
+                break;
+            }
+        }
+
+        boolean isValidUsername = username.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}") || username.matches("[a-zA-Z0-9_]+");
+
+        if (!isValidUsername){
+            req.setAttribute("message", "Invalid login name");
+            req.getRequestDispatcher("Authentication/registerHost.jsp").forward(req, resp);
+            return;
+        }
+
+        if (!isNumeric){
+            req.setAttribute("message", "Invalid phone number");
+            req.getRequestDispatcher("Authentication/registerHost.jsp").forward(req, resp);
+            return;
+        }
+
+        if (phone.length() < 9 || phone.length() > 15){
+            req.setAttribute("message", "Phone number must be between 9 and 15 digits");
+            req.getRequestDispatcher("Authentication/registerHost.jsp").forward(req, resp);
+            return;
+        }
+
+        if (password.length() < 6 || password.length() > 32) {
+            req.setAttribute("message", "Password from 6 - 32 characters");
+            req.getRequestDispatcher("Authentication/registerHost.jsp").forward(req, resp);
+            return;
+        }
+
         if (userService.checkEmail(username)) {
-            req.setAttribute("message","Account is duplicated");
-            req.getRequestDispatcher("registerHost.jsp").forward(req,resp);
-        } else if (!confirmPassword.equals(password)){
-            req.setAttribute("message","Passwords do not match");
-            req.getRequestDispatcher("registerHost.jsp").forward(req,resp);
-        }else {
+            req.setAttribute("message", "Account is duplicated");
+            req.getRequestDispatcher("Authentication/registerHost.jsp").forward(req, resp);
+        } else if (!confirmPassword.equals(password)) {
+            req.setAttribute("message", "Passwords do not match");
+            req.getRequestDispatcher("Authentication/registerHost.jsp").forward(req, resp);
+        } else {
             try {
                 userService.insertUser(newUser);
             } catch (ClassNotFoundException e) {
@@ -34,6 +71,9 @@ public class UserRegisterAsHost extends HttpServlet {
             }
             req.getRequestDispatcher("");
         }
-
     }
+
+
+
 }
+
